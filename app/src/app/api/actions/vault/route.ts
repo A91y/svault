9b72c1eb031dc26c051fd47548ff6d2c88be74de
A91y <65825207+A91y@users.noreply.github.com>
2024-09-16 00:20:12 +0530

    feat: added entry blink

---
+++ app/src/app/api/actions/vault/route.ts
@@ -0,0 +1,158 @@
+import {
+  ActionPostResponse,
+  createPostResponse,
+  ActionGetResponse,
+  ActionPostRequest,
+  createActionHeaders,
+  ActionError,
+} from "@solana/actions";
+import {
+  clusterApiUrl,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import { Program, Idl, AnchorProvider, setProvider } from "@coral-xyz/anchor";
+import { Vault } from "../../../../idl/vault";
+import idl from "../../../../idl/vault.json";
+import * as anchor from "@coral-xyz/anchor";
+
+// create the standard headers for this route (including CORS)
+const headers = createActionHeaders({
+  chainId: "devnet",
+});
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+const program = new Program<Vault>(
+  idl as Vault,
+  new AnchorProvider(connection, {} as any, {})
+);
+
+export const GET = async (req: Request) => {
+  try {
+    const requestUrl = new URL(req.url);
+    const baseHref = new URL(
+      `/api/actions`,
+      requestUrl.origin
+    ).toString();
+
+    // Create the payload with buttons for each action
+    const payload: ActionGetResponse = {
+      type: "action",
+      title: "Vault - The Vault",
+      icon: new URL("/logo.png", requestUrl.origin).toString(),
+      description: `A Vault to store Native SOLs: 
+      1. Create a Vault
+      2. Deposit into the Vault 
+      3. Withdraw from the Vault
+      4. Close the Vault 
+      `,
+      label: "Vault Actions",
+      links: {
+        actions: [
+          {
+            label: "Initialize Vault",
+            href: `${baseHref}/initialize`,
+          },
+          {
+            label: "Deposit",
+            href: `${baseHref}/deposit?amount={amount}`,
+            parameters: [
+              {
+                name: "amount",
+                type: "number",
+                label: "Amount to Deposit",
+              },
+            ],
+          },
+          {
+            label: "Withdraw",
+            href: `${baseHref}/withdraw?amount={amount}`,
+            parameters: [
+              {
+                name: "amount",
+                type: "number",
+                label: "Amount to Withdraw",
+              },
+            ],
+          },
+          {
+            label: "Close Vault",
+            href: `${baseHref}/close`,
+          },
+        ],
+      },
+    };
+
+    return Response.json(payload, {
+      headers,
+    });
+  } catch (err) {
+    console.log(err);
+    let actionError: ActionError = { message: "An unknown error occurred" };
+    if (typeof err == "string") actionError.message = err;
+    return Response.json(actionError, {
+      status: 400,
+      headers,
+    });
+  }
+};
+
+// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
+// THIS WILL ENSURE CORS WORKS FOR BLINKS
+export const OPTIONS = async () => Response.json(null, { headers });
+
+export const POST = async (req: Request) => {
+  try {
+    const body: ActionPostRequest = await req.json();
+
+    // validate the client provided input
+    let account: PublicKey;
+    try {
+      account = new PublicKey(body.account);
+    } catch (err) {
+      throw 'Invalid "account" provided';
+    }
+    const [statePda, stateBump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("state"), account.toBuffer()],
+      program.programId
+    );
+    const vaultState = statePda;
+
+    const [vaultPda, vaultBumpSeed] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("vault"), vaultState.toBuffer()],
+        program.programId
+      );
+    const vault = vaultPda;
+
+    const { blockhash, lastValidBlockHeight } =
+      await connection.getLatestBlockhash();
+
+    const transaction = new Transaction({
+      feePayer: account,
+      blockhash,
+      lastValidBlockHeight,
+    }).add();
+
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        transaction,
+        message: "Vault Initialized",
+      },
+    });
+    return Response.json(payload, {
+      headers,
+    });
+  } catch (err) {
+    console.log(err);
+    let actionError: ActionError = { message: "An unknown error occurred" };
+    if (typeof err == "string") actionError.message = err;
+    return Response.json(actionError, {
+      status: 400,
+      headers,
+    });
+  }
+};
