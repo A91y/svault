2cb48b3896ffd7353a44f7336d5c5e93c8cdfb38
A91y <65825207+A91y@users.noreply.github.com>
2024-09-16 00:36:46 +0530

    refactor: Update vault action URL in Home component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://dial.to/?action=https://localhost:3000/vault&cluster=devnet"
+            href="https://dial.to/?action=https://svault.ayushagr.me/vault&cluster=devnet"
             target="_blank"
             rel="noopener noreferrer"
           >
