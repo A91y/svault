572ccd27a7890a550fe9639f86a9b48b8ab26080
A91y <65825207+A91y@users.noreply.github.com>
2024-09-20 01:57:55 +0530

    feat: custom 404

---
+++ app/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to homepage
+    router.replace("/");
+  }, [router]);
+
+  return null;
+}
